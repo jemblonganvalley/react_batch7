0dd9627159119c8ab91db610dce9958e1fd45f89
jemblonganvalley <jemblonganvalley@gmail.com>
2020-11-24 04:21:53 +0700

    update kelas pagi

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,39 +1,16 @@
-import { useState } from "react";
-import Menu from "../menu/Menu";
-import Modal from "../modal/Modal";
 import "./Navbar.css";
 
 const Navbar = () => {
-  //STATE SHOW
-  const [show, setShow] = useState(false);
-  const [tabMenu, setTabMenu] = useState(false);
-
   return (
     <>
-      {/* TERNARY OPERATION */}
-      {show ? <Modal /> : null}
-
-      <Menu show={tabMenu} />
-
       <nav className="navbar">
         <h3 className="brand">JVALLEY</h3>
-        <span
-          className="material-icons menu"
-          onClick={() => {
-            setShow(!show);
-          }}
-        >
-          menu
-        </span>
-
-        <span
-          className="material-icons arrow"
-          onClick={() => {
-            setTabMenu(!tabMenu);
-          }}
-        >
-          keyboard_arrow_down
-        </span>
+        <div className="menu">
+          <span>home</span>
+          <span>about</span>
+          <span>contact</span>
+          <span>gallery</span>
+        </div>
       </nav>
     </>
   );
