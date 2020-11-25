8930b8d233c5eedcb1d7e4fa575694387b55265e
jemblonganvalley <jemblonganvalley@gmail.com>
2020-11-25 20:48:00 +0700

    update kelas pagi

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import "./App.css";
-import React from "react";
+//component adalah sebuah function
+//yang akan me Return Code JSX
 
+//syarat pambuatan component
+// 1. Nama File, Variable Harus Huruf Besar
+// 2. Bisa berupa file JS atau JSX
+// 3. Karena akan di import maka harus ada proses export
+// 4. Proses return hanya untuk satu enclosing tag
 const App = () => {
   return (
-    <main className="App" style={{ marginTop: "10vh" }}>
-      <h1>Hello</h1>
-    </main>
+    <div className="App">
+      <h3>OKE</h3>
+    </div>
   );
 };
 
