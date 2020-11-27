6dc6eb0324f02f0cc1bba061170a11cb718ab484
jemblonganvalley <jemblonganvalley@gmail.com>
2020-11-27 13:04:04 +0700

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,47 @@
 // 1 kita bisa langsung import css ke component
+import { useState } from "react";
 import "./Navbar.css";
 
 // 2 Kita buat function untuk mereturn components kita
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
-    <nav className="navbar">
-      <h3 className="brand">Andi Maulana</h3>
-      <div className="menu">
-        <span>home</span>
-        <span>about</span>
-        <span>contact</span>
-        <span>gallery</span>
-      </div>
-    </nav>
+    <>
+      <nav className="navbar">
+        <h3 className="brand">Andi Maulana</h3>
+        <i
+          className="material-icons menu_icon"
+          onClick={() => {
+            setShowMenu(!showMenu);
+          }}
+        >
+          menu
+        </i>
+
+        {/* CONDITIONAL RENDERING dengan TERNARY OPERATION*/}
+
+        {/* {showMenu === true ? <Menu show={showMenu} /> : null} */}
+      </nav>
+      <Menu show={showMenu} />
+    </>
+  );
+};
+
+//membuat component menu..
+const Menu = ({ show }) => {
+  return (
+    <div
+      className="menu"
+      style={{
+        animationName: show ? "down" : "up",
+      }}
+    >
+      <span>home</span>
+      <span>about</span>
+      <span>contact</span>
+      <span>gallery</span>
+    </div>
   );
 };
 
