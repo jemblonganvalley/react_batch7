ae7a2311fc1a08096fd629fbb422cd434c902c3e
jemblonganvalley <jemblonganvalley@gmail.com>
2020-12-02 09:42:59 +0700

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Menu from "../menu/Menu";
 import Modal from "../modal/Modal";
 import "./Navbar.css";
+import Logo from "../../assets/logo512.png";
 
 const Navbar = () => {
   //STATE SHOW
@@ -16,6 +17,7 @@ const Navbar = () => {
       <Menu show={tabMenu} />
 
       <nav className="navbar">
+        <img src={Logo} alt="logo fadliselaz" width="40px" />
         <h3 className="brand">JVALLEY</h3>
         <span
           className="material-icons menu"
