e9a6a1ee80ff9c0124412a17e0a5e92a9e6a2e2b
jemblonganvalley <jemblonganvalley@gmail.com>
2020-12-04 15:40:19 +0700

    update comment

--- src/components/section/pages/about/About.jsx
+++ src/components/section/pages/about/About.jsx
@@ -1,11 +1,59 @@
 //1 Import css
+import { useEffect, useState } from "react";
 import "./About.css";
 
 //2 buat component Home
 const About = () => {
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users", {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  }, []);
+
+  const [users, setUsers] = useState();
+
   return (
     <section className="about" id="about">
       <h1>About Page</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>username</th>
+            <th>email</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {users ? (
+            <>
+              {users.map((e) => {
+                return (
+                  <tr key={e.id}>
+                    <td>{e.id}</td>
+                    <td>{e.name}</td>
+                    <td>{e.username}</td>
+                    <td>{e.email}</td>
+                  </tr>
+                );
+              })}
+            </>
+          ) : (
+            <tr>
+              <td>wait</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </section>
   );
 };
--- src/components/section/pages/contact/Contact.jsx
+++ src/components/section/pages/contact/Contact.jsx
@@ -1,11 +1,49 @@
 //1 Import css
+import { useEffect, useState } from "react";
 import "./Contact.css";
 
 //2 buat component Contact
 const Contact = () => {
+  //kita gunakan lifecycle useEffect
+  useEffect(() => {
+    //function di dalam sini akan berjalan sebelum component terload
+    // fetch('endpoint', {config})
+    fetch("https://a796badd1fb8.ngrok.io/contact", {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMsg(data);
+      });
+  }, []);
+
+  const [msg, setMsg] = useState();
+
   return (
     <section className="contact" id="contact">
       <h1>Contact Page</h1>
+
+      <div className="message_area">
+        {msg ? (
+          <>
+            {msg.map((e) => {
+              return (
+                <div key={e.id} className="card">
+                  <h3>{e.from}</h3>
+                  <p>{e.message}</p>
+                </div>
+              );
+            })}
+          </>
+        ) : (
+          <span>Wait...</span>
+        )}
+      </div>
     </section>
   );
 };
